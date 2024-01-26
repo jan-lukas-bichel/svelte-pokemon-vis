c96c504491c43d65904deb07debc08077d0990a7
Jan <jlbichel@gmail.com>
2024-01-26 20:51:58 +0100

    Shorter implementation, reading csv with d3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,22 @@
 <script>
-  import papa from 'papaparse'
-	import * as d3 from "d3"
-  let promise
+  import * as d3 from "d3";
+  let promise;
 
   async function getPokemon() {
-    const res = await fetch('http://localhost:5173/pokemon.csv', {
-    headers: {
-        'Content-Type': 'text/csv'
-    }})
-    let csv_data = await res.text()
-    let parsed_csv = papa.parse(csv_data)
-    return parsed_csv
+    const res = await d3.csv("http://localhost:5173/pokemon.csv");
+    return res;
   }
-  promise = getPokemon()
+  promise = getPokemon();
 </script>
 
 {#await promise}
-	<p>...loading dataset</p>
+  <p>...loading dataset</p>
 {:then data}
   <ul>
-    {#each data.data as pokemon}
-    <li>{pokemon}</li>
+    {#each data as pokemon}
+      <li>{pokemon.name}</li>
     {/each}
   </ul>
 {:catch error}
-	<p style="color: red">{error.message}</p>
+  <p style="color: red">{error.message}</p>
 {/await}
